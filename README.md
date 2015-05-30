a8523552b3b36a5c68835a38bd6382b01616ce03
Bunny <w@bunnywong.com>
2015-05-30 09:17:11 +0800

    [ Sign up ] Add with radio & drop-down issue

---
+++ README.md
@@ -0,0 +1,6 @@
+# KOL
+
+TODO:
+	Sign up now
+	- Switch on 'disable' class
+	- Radio: blue outline
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
 console.log('\'Allo \'Allo!');
+
+$('.js-step-1 button').on('click', function() {
+	// 1. Do validator
+	// 1.1 Show error
+	// 1.2.1 Remove tab disable
+	// 1.2.2 Jump to next tab
+})
