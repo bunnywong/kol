788e1401ca3ad582b80547da111742e22c47d3ae
Bunny <w@bunnywong.com>
2015-06-01 01:31:54 +0800

    [ Sign up ] Add step 2, 3 and finsih

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,18 @@
 console.log('\'Allo \'Allo!');
 
-$('.js-step-1 button').on('click', function() {
+$('.js-step-1-continue').on('click', function() {
 	// 1. Do validator
 	// 1.1 Show error
 	// 1.2.1 Remove tab disable
+	$('.js-step-2-nav').removeClass('disable');
+
 	// 1.2.2 Jump to next tab
-})
+	$('.js-step-2-nav').trigger('click');
+});
+
+
+// DEBUG
+$('.js-step-2-nav').trigger('click');
+
+
+// $('.hashtag').tagsinput('items')
